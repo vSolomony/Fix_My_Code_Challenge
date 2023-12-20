bb9741b157df2536d1d7ee46285a1bbb98ade760
vSolomony <mohamed.a.kassem95@gmail.com>
2023-12-20 08:04:19 -0800

    123

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -17,9 +17,10 @@ if (process.argv.length <= 2) {
 const size = parseInt(process.argv[2], 10);
 
 for (let i = 0; i < size; i++) {
+    let row = '';
     for (let j = 0; j < size; j++) {
-        process.stdout.write("#");
+        row += '#';
     }
-    process.stdout.write("\n");
+    console.log(row);
 }
 
