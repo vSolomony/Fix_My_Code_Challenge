15240980ea4c78bc8abc6441353bead278e7c030
vSolomony <mohamed.a.kassem95@gmail.com>
2023-12-20 08:11:18 -0800

    123

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -8,16 +8,17 @@ def fizzbuzz(n):
     """
     FizzBuzz function prints numbers from 1 to n separated by a space.
 
-    - For multiples of three print "Fizz" instead of the number and for
-      multiples of five print "Buzz".
+    - For multiples of three print "Fizz" instead of the number.
+    - For multiples of five print "Buzz" instead of the number.
     - For numbers which are multiples of both three and five print "FizzBuzz".
+    - Otherwise, print the number.
     """
     if n < 1:
         return
 
     tmp_result = []
     for i in range(1, n + 1):
-        if (i % 3 == 0) and (i % 5 == 0):
+        if i % 3 == 0 and i % 5 == 0:
             tmp_result.append("FizzBuzz")
         elif i % 3 == 0:
             tmp_result.append("Fizz")
@@ -28,7 +29,7 @@ def fizzbuzz(n):
     print(" ".join(tmp_result))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     if len(sys.argv) <= 1:
         print("Missing number")
         print("Usage: ./0-fizzbuzz.py <number>")
@@ -37,3 +38,5 @@ if __name__ == "__main__":
 
     number = int(sys.argv[1])
     fizzbuzz(number)
+
+
