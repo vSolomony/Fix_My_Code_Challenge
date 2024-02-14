cac95738b94021ecc1c1cd047a579730605aebc7
vSolomony <mohamed.a.kassem95@gmail.com>
2024-02-13 18:36:57 -0800

    123

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def area(self):
+        """ Area of the square """
+        return self.width * self.width
+
+    @property
+    def perimeter(self):
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        return "{}/{}".format(self.width, self.height)
+
+if __name__ == "__main__":
+    s = Square(width=12, height=9)
+    print(s)
+    print(s.area)
+    print(s.perimeter)
+
